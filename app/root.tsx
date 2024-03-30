dde34717b5df7f1517daed68acd754f616232bc5
stephenhackettdelaney <stephenhackettdelaney@gmail.com>
2024-03-30 09:18:41 -0700

    copy update + start seo

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,73 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
+
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: "HolyCat" },
+//     {
+//       property: "og:title",
+//       content: "HolyCat",
+//     },
+//     {
+//       name: "description",
+//       content: "This app is the best",
+//     },
+//     { title: "Stephen Hackett-Delaney" },
+//     { description: "Software engineer portfolio" },
+//     { keywords: ['Stephen Hackett-Delaney', 'Stephen hd', 'stephenhd', 'Next.js', 'React', 'JavaScript', 'web design', 'website design', 'website builder'] },
+//     { authors: [{ name: 'Stephen Hackett-Delaney', url: 'https://www.stephenhd.com/' }] },
+//     { creator: 'Stephen Hackett-Delaney' },
+//     { publisher: 'Stephen Hackett-Delaney' },
+//     {
+//       openGraph: {
+//         title: "Stephen Hackett-Delaney",
+//         description: "Software engineer portfolio",
+//         url: 'https://www.stephenhd.com/',
+//         siteName: 'Stephen Hackett-Delaney',
+//         images: [
+//           {
+//             url: 'https://www.stephenhd.com/opengraph-image.png', // Must be an absolute URL
+//             width: 1200,
+//             height: 630,
+//           },
+//         ],
+//         locale: 'en_US',
+//         type: 'website',
+//       }
+//     },
+//     {
+//       twitter: {
+//         card: 'summary_large_image',
+//         title: 'Stephen Hackett-Delaney',
+//         description: 'Software engineer portfolio',
+//         creator: 'Stephen Hackett-Delaney',
+//         images: ['https://www.stephenhd.com/twitter-image.png'], // Must be an absolute URL
+//       }
+//     },
+//     {
+//       robots: {
+//         index: false,
+//         follow: true,
+//         nocache: true,
+//         googleBot: {
+//           index: true,
+//           follow: false,
+//           noimageindex: true,
+//           'max-video-preview': -1,
+//           'max-image-preview': 'large',
+//           'max-snippet': -1,
+//         },
+//       }
+//     },
+//   ];
+// };
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import TwitterX from "~/icons/twitterX";
-import Facebook from "~/icons/facebook";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,48 +11,41 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex justify-center md:items-center  bg-grey/70 md:h-screen">
-      <div className="flex flex-col gap-[75px] max-w-4xl text-brown p-16 bg-grey">
-        <section>
-          <section className="grid md:grid-cols-2 gap-24">
-            <img className="rounded p-5 bg-brown/10" src="/holy_cat_frame.png" alt="holy cat mascot" />
-            <div className="flex flex-col gap-6">
-              <h1 className="text-5xl font-serif">Holy Cat</h1>
-              <div className="h-1 w-full rounded-full bg-brown" />
-              <p className="text-2xl font-serif">A cool crypto currency</p>
-              <p className="text-base leading-[32px]">Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-            </div>
-          </section>
+    <div className="flex justify-center md:items-center bg-grey/50 md:h-screen p-4 md:p-8 ">
+      <div className="flex flex-col gap-[75px] max-w-4xl text-brown p-16 bg-grey shadow-lg">
+        <section className="grid md:grid-cols-2 justify-items-center items-center gap-24">
+          <img className="rounded p-5 bg-brown/10" src="/holy_cat_frame.png" alt="holy cat mascot" />
+          <div className="flex flex-col gap-6">
+            <h1 className="text-5xl font-serif">HolyCat</h1>
+            <div className="h-1 w-full rounded-full bg-brown" />
+            {/* <p className="text-2xl font-serif">A cool crypto currency</p> */}
+            <p className="text-base leading-[32px]">HolyCat is the holiest cat of them all. He sits on his cross, arms outstretched, patiently waiting for his followers to bow to him. Support HolyCat as he spreads good across the world.</p>
+          </div>
         </section>
-        <section>
-          <section className="grid md:grid-cols-2 gap-24">
-            <div className="flex flex-col gap-6">
-              <h1 className="text-5xl font-serif">Tokenomics</h1>
-              <div className="h-1 w-full rounded-full bg-brown" />
-              <p className="text-2xl font-serif">What this coin offers</p>
-              <ul className="flex flex-col gap-4 pl-4 list-disc">
-                <li>0/0 tax</li>
-                <li>Liquidity pool burnt</li>
-                <li>Immutable</li>
-                <li>100% community owned</li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-6">
-              <h1 className="text-5xl font-serif">Find us here</h1>
-              <div className="h-1 w-full rounded-full bg-brown" />
-              <section className="flex flex-1 flex-col text-brown">
-                <div className="flex flex-1 items-center gap-8 md:justify-end">
+        <section className="grid md:grid-cols-2 gap-24">
+          <div className="flex flex-col gap-6">
+            <h1 className="text-4xl font-serif">Tokenomics</h1>
+            <div className="h-1 w-full rounded-full bg-brown" />
+            <p className="text-2xl font-serif">What this coin offers</p>
+            <ul className="flex flex-col gap-4 pl-4 list-disc">
+              <li>No tax</li>
+              <li>Liquidity pool burnt</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-6 ">
+            <h1 className="text-4xl font-serif">Find us here</h1>
+            <div className="h-1 w-full rounded-full bg-brown" />
+            <section className="flex flex-1 flex-col text-brown">
+              <div className="flex flex-1 items-center gap-8 md:justify-end">
+                <Link to="https://x.com/holy_cat80?t=Z5g_QxYnKtWp1TPwisv_eg&s=09" target="_blank" rel="noreferrer">
                   <TwitterX className="w-8 h-8" />
-                  <TwitterX className="w-8 h-8" />
-                  <TwitterX className="w-8 h-8" />
-                  <TwitterX className="w-8 h-8" />
-                </div>
-                <p className="mt-16 md:mt-auto md:text-right">@2024 - Holy Cat</p>
-              </section>
-            </div>
+                </Link>
+              </div>
+            </section>
+          </div>
+        </section>
+        <p className="text-center text-xs">@2024 - Holy Cat</p>
 
-          </section>
-        </section>
       </div>
 
     </div>
