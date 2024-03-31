9722e75354155285ca95af7edf86181c0006073e
stephenhackettdelaney <stephenhackettdelaney@gmail.com>
2024-03-30 17:11:33 -0700

    add icons etc..

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import TwitterX from "~/icons/twitterX";
+import TelegramIcon from "~/icons/telegram";
+import DexScreenerIcon from "~/icons/dexscreener";
 import { Link } from "@remix-run/react";
 import { MetaFunction } from "@remix-run/node";
 
@@ -45,7 +47,7 @@ export default function Index() {
           <div className="flex flex-col gap-6">
             <h1 className="text-4xl font-serif">HolyCat</h1>
             <div className="h-1 w-full rounded-full bg-brown" />
-            {/* <p className="text-2xl font-serif">A cool crypto currency</p> */}
+            <p className="text-2xl font-serif">He's just so holy!</p>
             <p className="text-base leading-[32px]">HolyCat is the holiest cat of them all. He sits on his cross, arms outstretched, patiently waiting for his followers to bow to him. Support HolyCat as he spreads good across the world.</p>
           </div>
         </section>
@@ -56,6 +58,7 @@ export default function Index() {
             <ul className="flex flex-col gap-4 pl-4 list-disc">
               <li>No tax</li>
               <li>Liquidity pool burnt</li>
+              <li>1,000,000,000 supply</li>
             </ul>
           </div>
           <div className="flex flex-col gap-6 ">
@@ -65,6 +68,12 @@ export default function Index() {
               <div className="flex flex-1 items-center gap-8 md:justify-end">
                 <Link to="https://x.com/holy_cat80?t=Z5g_QxYnKtWp1TPwisv_eg&s=09" target="_blank" rel="noreferrer">
                   <TwitterX className="w-8 h-8" />
+                </Link>
+                <Link to="https://x.com/holy_cat80?t=Z5g_QxYnKtWp1TPwisv_eg&s=09" target="_blank" rel="noreferrer">
+                  <TelegramIcon className="w-8 h-8" />
+                </Link>
+                <Link to="https://x.com/holy_cat80?t=Z5g_QxYnKtWp1TPwisv_eg&s=09" target="_blank" rel="noreferrer">
+                  <DexScreenerIcon className="w-8 h-8" />
                 </Link>
               </div>
             </section>
